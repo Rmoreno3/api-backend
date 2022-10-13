1d39eb5291a4d24aee1e977b2a24dbf558d3987c
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-10-13 15:57:20 -0400

    fix errors 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const logger = require('../loggerMiddleware')
+const logger = require('./loggerMiddleware')
 
 app.use(cors())
 app.use(express.json())
@@ -95,7 +95,5 @@ app.use((request, response) => {
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running o port ${PORT}`)
 })
-
-module.exports = app
