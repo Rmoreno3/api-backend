447c8f489ce34863e4958af6c6470076ae23f7af
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-12-13 00:33:31 -0400

    add time expirete token, fix errorHandle

--- controllers/routesLogin.js
+++ controllers/routesLogin.js
@@ -6,29 +6,33 @@ const loginUser = async (request, response, next) => {
   const { body } = request
   const { username, password } = body
 
-  const user = await User.findOne({ username })
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(password, user.passwordHash)
+  try {
+    const user = await User.findOne({ username })
+    const passwordCorrect = user === null
+      ? false
+      : await bcrypt.compare(password, user.passwordHash)
 
-  if (!(user && passwordCorrect)) {
-    response.status(401).json({
-      error: 'invalid user or password'
+    if (!(user && passwordCorrect)) {
+      response.status(401).json({
+        error: 'invalid user or password'
+      })
+    }
+
+    const userForToken = {
+      id: user._id,
+      username: user.username
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: '7d' })
+
+    response.send({
+      name: user.name,
+      username: user.username,
+      token
     })
+  } catch (e) {
+    next()
   }
-
-  const userForToken = {
-    id: user._id,
-    username: user.username
-  }
-
-  const token = jwt.sign(userForToken, process.env.SECRET)
-
-  response.send({
-    name: user.name,
-    username: user.username,
-    token
-  })
 }
 
 module.exports = loginUser
